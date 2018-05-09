136983862c7a73992bd9ba7e25b40f5dacb989d8
Yasu_umiNishikawa <yasu.umi.19910101@gmail.com>
2018-05-09 09:51:56 +0900

    update example

--- examples/test.rs
+++ examples/test.rs
@@ -4,20 +4,28 @@ use cabocha::Parser;
 
 fn main() {
     let mut parser = Parser::new("");
+    let text = "一郎は二郎が描いた絵を三郎に贈った。";
 
-    let text1 = parser.parse_to_str("一郎は二郎が描いた絵を三郎に贈った。");
-    println!("{}", text1);
+    println!("{}", parser.parse_to_str(text));
 
-    let tree = parser.parse_to_tree("一郎は二郎が描いた絵を三郎に贈った。");
-    println!("{}", tree.to_string(cabocha::CABOCHA_FORMAT::CONLL));
+    let tree = parser.parse_to_tree(text);
+    println!("{}", tree.to_string(cabocha::CABOCHA_FORMAT::LATTICE));
 
-    let tokens = tree.token_iter();
-    for token in tokens {
-        println!("{:?}", token);
-    }
-
-    let chunks = tree.chunk_iter();
-    for chunk in chunks {
-        println!("{:?}", chunk);
+    for (idx, chunk) in tree.chunk_iter().enumerate() {
+        println!(
+            "* {} {}D {}/{} {}",
+            idx,
+            chunk.link(),
+            chunk.head_pos(),
+            chunk.func_pos(),
+            chunk.score(),
+        );
+        let token_pos = chunk.token_pos();
+        for i in token_pos..(token_pos + chunk.token_size()) {
+            match tree.token(i) {
+                Some(token) => println!("{}\t{}", token.surface(), token.feature()),
+                None => (),
+            }
+        }
     }
 }
